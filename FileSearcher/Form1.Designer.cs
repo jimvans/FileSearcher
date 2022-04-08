66762ecee7b8dd4b6ba7a3dbdd8578d5e1f70656
Jim Filbert Vano <jvano@1-stop.biz>
2022-04-08 11:37:43 +0800

    Added project

---
+++ FileSearcher/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿
+namespace FileSearcher
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lstProgress = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.chkDelete = new System.Windows.Forms.CheckBox();
+            this.chkClear = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(28, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input Directory :";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtInput.Location = new System.Drawing.Point(188, 25);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(457, 29);
+            this.txtInput.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(13, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Output Directory :";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtOutput.Location = new System.Drawing.Point(188, 68);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(457, 29);
+            this.txtOutput.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(103, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Search :";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtSearch.Location = new System.Drawing.Point(188, 112);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(555, 29);
+            this.txtSearch.TabIndex = 5;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnStart.Location = new System.Drawing.Point(188, 199);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(128, 49);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnStop.Location = new System.Drawing.Point(322, 199);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(128, 49);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnClose.Location = new System.Drawing.Point(615, 199);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(128, 49);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lstProgress
+            // 
+            this.lstProgress.BackColor = System.Drawing.Color.Gainsboro;
+            this.lstProgress.FormattingEnabled = true;
+            this.lstProgress.ItemHeight = 15;
+            this.lstProgress.Location = new System.Drawing.Point(188, 265);
+            this.lstProgress.Name = "lstProgress";
+            this.lstProgress.ScrollAlwaysVisible = true;
+            this.lstProgress.Size = new System.Drawing.Size(555, 169);
+            this.lstProgress.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(185, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(306, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "* To search multiple words place a comma to each word.";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(87, 265);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Progress :";
+            // 
+            // chkDelete
+            // 
+            this.chkDelete.AutoSize = true;
+            this.chkDelete.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.chkDelete.Location = new System.Drawing.Point(188, 163);
+            this.chkDelete.Name = "chkDelete";
+            this.chkDelete.Size = new System.Drawing.Size(212, 25);
+            this.chkDelete.TabIndex = 12;
+            this.chkDelete.Text = "Delete files not matched.";
+            this.chkDelete.UseVisualStyleBackColor = true;
+            // 
+            // chkClear
+            // 
+            this.chkClear.AutoSize = true;
+            this.chkClear.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.chkClear.Location = new System.Drawing.Point(443, 163);
+            this.chkClear.Name = "chkClear";
+            this.chkClear.Size = new System.Drawing.Size(175, 25);
+            this.chkClear.TabIndex = 13;
+            this.chkClear.Text = "Clean output folder.";
+            this.chkClear.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(651, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 29);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Select folder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(651, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 29);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Select folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 448);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chkClear);
+            this.Controls.Add(this.chkDelete);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lstProgress);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label4);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "File Searcher and Sorter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.ListBox lstProgress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox chkDelete;
+        private System.Windows.Forms.CheckBox chkClear;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ FileSearcher/Form1.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileSearcher
+{
+    public partial class frmMain : Form
+    {
+        private const string APP_NAME = "File Searcher and Sorter";
+        public frmMain()
+        {
+            InitializeComponent();
+            //this.setDefaultValues();
+        }
+
+        private void setDefaultValues()
+        {
+            this.txtInput.Text = "D:\\repository\\dotnet\\Test Data\\input";
+            this.txtOutput.Text = "D:\\repository\\dotnet\\Test Data\\output";
+            this.txtSearch.Text = "MEDU6161670, 227873504";
+            this.chkDelete.Checked = false;
+            this.chkClear.Checked = true;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(this.txtInput.Text))
+                MessageBox.Show("Input Directory is required!", APP_NAME, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (string.IsNullOrWhiteSpace(this.txtOutput.Text))
+                MessageBox.Show("Output Directory is required!", APP_NAME, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else if (string.IsNullOrWhiteSpace(this.txtSearch.Text))
+                MessageBox.Show("Search text is required!", APP_NAME, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                if (this.chkClear.Checked)
+                    this.clearOutputFolder();
+
+                this.lstProgress.Items.Clear();
+                this.btnStart.Enabled = false;
+                this.btnStop.Enabled = true;
+                this.process();
+            }
+            this.btnStart.Enabled = true;
+            this.btnStop.Enabled = false;
+        }
+
+        private void clearOutputFolder()
+        {
+            var directory = new DirectoryInfo(this.txtOutput.Text);
+            foreach (var file in directory.GetFiles()) file.Delete();
+            foreach (var subDirectory in directory.GetDirectories()) subDirectory.Delete(true);
+        }
+
+        private void process()
+        {
+            var searchStrings = this.txtSearch.Text.Split(",");
+
+            foreach (string file in Directory.EnumerateFiles(this.txtInput.Text, ""))
+            {
+                this.lstProgress.Items.Add(file);
+                this.lstProgress.SelectedIndex = this.lstProgress.Items.Count - 1;
+
+                var fileContents = File.ReadAllText(file);
+
+                foreach (var searchStr in searchStrings)
+                {
+                    if (fileContents.Contains(searchStr.Trim()))
+                    {
+                        var destination = this.txtOutput.Text + "\\" + Path.GetFileName(file);
+                        //Copy File to Output
+                        File.Copy(file, destination);
+
+                        if (this.chkDelete.Checked)
+                            File.Delete(file);
+                    }
+                }
+
+                Application.DoEvents();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                this.txtInput.Text = dialog.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                this.txtOutput.Text = dialog.FileName;
+            }
+        }
+    }
+}
